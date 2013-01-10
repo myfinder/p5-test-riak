94271c4098d96f2dbc03c5bfaedd421cae5d87a6
Tatsuro Hisamori <medianetworks@gmail.com>
2013-01-10 18:33:46 +0900

    initial commit

---
+++ lib/Test/riak.pm
@@ -0,0 +1,32 @@
+package Test::riak;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::riak -
+
+=head1 SYNOPSIS
+
+  use Test::riak;
+
+=head1 DESCRIPTION
+
+Test::riak is
+
+=head1 AUTHOR
+
+Tatsuro Hisamori E<lt>myfinder@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
